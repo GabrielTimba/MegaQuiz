c55a9c2f67cd0a930bfd28f519fd500224f68623
Gabriel Timba <gabrieltimba@hotmail.com>
2020-07-03 23:54:40 +0200

    add content in quiz page and start footer componet

--- frotend/src/components/footer/index.tsx
+++ frotend/src/components/footer/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {FiHeart} from 'react-icons/fi'
 
 import './style.css';
 
 const Footer=()=>{
     return(
         <footer>
+            <h4>Desenvolvido a mao & com </h4> 
+            <FiHeart size={16} color="#fff"/>
+            <h4> por Gabriel Timba</h4>
         </footer>
     )
 }
--- frotend/src/pages/Quiz/index.tsx
+++ frotend/src/pages/Quiz/index.tsx
@@ -1,16 +1,73 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/footer';
+import Button from '../../components/Button';
+
+import './style.css'
 
 const Quiz=()=>{
     return(
-        <>
+        <div id="quiz-page">
             <Header/>
-            <main>
-                Conteudo
+            <main className="content">
+                <h4>Questoes Respondidas: 2/9</h4>
+                <div>
+                    <h3>1.What does CSS stand for?</h3>
+                    <ul>
+                        <li><span>A</span> Computer Style Sheets</li>
+                        <li><span>B</span> Computer Style Sheets</li>
+                        <li><span>C</span> Computer Style Sheets</li>
+                        <li><span>D</span> Computer Style Sheets</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>1.What does CSS stand for?</h3>
+                    <ul>
+                        <li><span>A</span> Computer Style Sheets</li>
+                        <li><span>B</span> Computer Style Sheets</li>
+                        <li><span>C</span> Computer Style Sheets</li>
+                        <li><span>D</span> Computer Style Sheets</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>1.What does CSS stand for?</h3>
+                    <ul>
+                        <li><span>A</span> Computer Style Sheets</li>
+                        <li><span>B</span> Computer Style Sheets</li>
+                        <li><span>C</span> Computer Style Sheets</li>
+                        <li><span>D</span> Computer Style Sheets</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>1.What does CSS stand for?</h3>
+                    <ul>
+                        <li><span>A</span> Computer Style Sheets</li>
+                        <li><span>B</span> Computer Style Sheets</li>
+                        <li><span>C</span> Computer Style Sheets</li>
+                        <li><span>D</span> Computer Style Sheets</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>1.What does CSS stand for?</h3>
+                    <ul>
+                        <li><span>A</span> Computer Style Sheets</li>
+                        <li><span>B</span> Computer Style Sheets</li>
+                        <li><span>C</span> Computer Style Sheets</li>
+                        <li><span>D</span> Computer Style Sheets</li>
+                    </ul>
+                </div>
+
+                <section>
+                    <Button text="Anterior"/>
+                    <Button text="Proximo"/>
+                </section>
             </main>
             <Footer/>
-        </>
+        </div>
     )
 }
 
