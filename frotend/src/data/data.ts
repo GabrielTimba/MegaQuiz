e311ae5927cef68f53a86a266afcc0aeca5fab4c
Gabriel Timba <gabrieltimba@hotmail.com>
2020-07-09 23:45:11 +0200

    adding favicon

--- frotend/src/data/data.ts
+++ frotend/src/data/data.ts
@@ -1,7 +1,7 @@
 export default [
     {
         number:1,
-        question: 'Obstetra e o profissional da saude que cuida .. ',
+        question: 'Obstetra é o profissional da saude que cuida .. ',
         answers: ['Da Pele', 'De Gestantes', 'Da Visao', 'De Fracturas e Lesoes'],
         correct: 2
     },
@@ -19,73 +19,73 @@ export default [
     },
     {
         number:4,
-        question: 'Vasco da Gama entrou em Mocambique a partir ..',
-        answers: ['De Inhambane', 'Ilha De Mocambique', 'Da Beira', 'Nacala Porto'],
+        question: 'Vasco da Gama entrou em Moçambique a partir ..',
+        answers: ['De Inhambane', 'Ilha De Moçambique', 'Da Beira', 'Nacala Porto'],
         correct: 2
     },
     {
         number:5,
-        question: 'Quem foi o primeiro navegador Portugues a chegar a Mocambique',
+        question: 'Quem foi o primeiro navegador Portugues a chegar a Moçambique',
         answers: ['Vasco da Gama', 'Luis Camoes', 'Pedro Alvares Cabral', 'Cristovao Colombo'],
         correct: 1
     },
     {
         number:6,
-        question: 'Quem e o actual Presidente da Russia',
+        question: 'Quem é o actual Presidente da Russia',
         answers: ['Jair Bolsonaro', 'Kim Jong-Un', 'Vladimir Putin', 'Gabriel Timba'],
         correct: 3
     },
     {
         number:7,
-        question: '"Lagrimas Nas Ondas Di Mar" e um filme realizado por ...',
+        question: '"Lagrimas Nas Ondas Di Mar" é um filme realizado por ...',
         answers: ['Sol de Carvalho', 'Mickey Fonseca', 'Julio da Silva', 'Mascarenhas Gotine'],
         correct: 3
     },
     {
         number:8,
-        question: 'Que Pais venceu o campeonato Africano das Nacoes em 2019',
-        answers: ['Argelia', 'Tunisia', 'Nigeria', 'Sinegal'],
+        question: 'Que País venceu o campeonato Africano das Nacoes em 2019',
+        answers: ['Argélia', 'Tunisia', 'Nigéria', 'Sinegal'],
         correct: 1
     },
     {
         number:9,
-        question: 'Que Pais foi Campeao Mundial de Futebol em 2018',
+        question: 'Que País foi Campeao Mundial de Futebol em 2018',
         answers: ['Portugar', 'Alemanha', 'Espanha', 'Franca'],
         correct: 4
     },
     {
         number:10,
-        question: 'Mafambisse e um Posto Adinistrativo localizado no distrito de ',
+        question: 'Mafambisse é um Posto Adinistrativo localizado no distrito de ',
         answers: ['Dondo', 'Nhamatanda', 'Marromeu', 'Marringue'],
         correct: 1
     },
     {
         number:11,
-        question: 'Machipanda e um posto Adinistrativo localizado no distrito de...',
+        question: 'Machipanda é um posto Adinistrativo localizado no distrito de...',
         answers: ['Sussundenga', 'Manica', 'Gondola', 'Vanduzi'],
         correct: 2
     },
     {
         number:12,
-        question: 'Quem foi o primeiro ministro do trabalho de Mocambique independente?',
+        question: 'Quem foi o primeiro ministro do trabalho de Moçambique independente?',
         answers: ['Armando Panguane', 'Jossias Mupandza', 'Jose Oscar Monteiro', 'Mariano Matsinhe'],
         correct: 4
     },
     {
         number:13,
-        question: '"O ultimo voo do Flamingo" e um filme realizado por..',
+        question: '"O Último voo do Flamingo" é um filme realizado por..',
         answers: ['Mia Couto', 'Pipas Forjaz', 'Joao Ribeiro', 'Lucinio Azevedo'],
         correct: 3
     },
     {
         number:14,
-        question: '"O Grande Bazar" e um filme realizado por..',
+        question: '"O Grande Bazar" é um filme realizado por..',
         answers: ['Folege Ricardo', 'Lucinio Azevedo', 'Jose Cardoso', 'ISabel Noronha'],
         correct: 2
     },
     {
         number:15,
-        question: 'O Bairro Albazine na cidade de Maput faz parte do distrito Urbano ...',
+        question: 'O Bairro Albazine na cidade de Maputo faz parte do distrito Urbano ...',
         answers: ['Katembe', 'Kamubukwana', 'Kamavota', 'Kanyaka'],
         correct: 3
     },
@@ -97,20 +97,20 @@ export default [
     },
     {
         number:17,
-        question: 'Quem foi o primeiro Ministro da saude de Mocambique Independente?',
-        answers: ['Helder Martins', 'Aida Libombo', 'Monica Macamo', 'Ivo Garrido'],
+        question: 'Quem foi o primeiro Ministro da saude de Moçambique Independente?',
+        answers: ['Helder Martins', 'Aida Libombo', 'Mónica Macamo', 'Ivo Garrido'],
         correct: 1
     },
     {
         number:18,
-        question: 'Em que clube joga o Atleta Mocambicano Mexer',
+        question: 'Em que clube joga o Atleta Moçambicano Mexer',
         answers: ['Nacional Madeira', 'Granada FC', 'Rennes FC', 'FC Bordeus'],
         correct: 4
     },
     {
         number:19,
         question: 'Sobre que parte do corpo humano incide o trabalho do Otorrinolaringologista',
-        answers: ['Cabeca', 'Barriga', 'Corracao', 'Osssos'],
+        answers: ['Cabeça', 'Barriga', 'Coração', 'Osssos'],
         correct: 1
     }
     
